99a7bab921328c43e102a01e4bdc97c1d7c36fdb
Ilya Menshikov <unkmas@gmail.com>
2016-12-11 18:20:05 +0500

    Add Requisites spec

---
+++ spec/lib/methods/requisites_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Focus::Methods::Requisites do
+  let(:http_client) { double :http_client }
+  let(:requisites) { described_class.new(http_client) }
+
+  it { expect(requisites.class.method_name).to eq 'req' }
+
+  describe '#get' do
+    let(:get) { requisites.get(**attributes) }
+
+    context 'when everything is ok' do
+      let(:attributes) do
+        {inn: 'company1_inn', ogrn: ['company2_ogrn', 'company3_ogrn']}
+      end
+
+      let(:converted) do
+        {method: 'req', inn: ['company1_inn'], ogrn: ['company2_ogrn', 'company3_ogrn']}
+      end
+
+      after { get }
+
+      it { expect(http_client).to receive(:get).with(converted) }
+    end
+
+    context 'when no requisites passed' do
+      let(:attributes) do
+        {}
+      end
+
+      it { expect { get }.to raise_error(ArgumentError) }
+    end
+  end
+end
